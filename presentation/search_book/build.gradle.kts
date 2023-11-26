783b7a783a006c9e409e8588034e97e98981d8a1
SangYeong Lee <lsy2014@gmail.com>
2023-11-26 23:03:54 +0900

    search_book module 추가

---
+++ presentation/search_book/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id(Plugins.Module.androidLibrary)
+    id(Plugins.Module.kotlinAndroid)
+    id(Plugins.Module.kotlinKapt)
+    id(Plugins.Module.hiltAndroid)
+}
+
+android {
+    compileSdk = AppConfig.compileSdk
+
+    defaultConfig {
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+    }
+
+    compileOptions {
+        sourceCompatibility = AppConfig.javaVersion
+        targetCompatibility = AppConfig.javaVersion
+    }
+    kotlinOptions {
+        jvmTarget = AppConfig.javaVersion.toString()
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Dep.Compose.compilerVersion
+    }
+}
+
+dependencies {
+    implementation(project(":domain"))
+    api(project(":presentation:core"))
+
+    Dep.Compose.apply {
+        implementation(ui)
+        implementation(tool)
+        implementation(material)
+    }
+
+    Dep.Navigation.apply {
+        implementation(compose)
+        implementation(hiltCompose)
+    }
+
+    Dep.Hilt.apply {
+        implementation(android)
+        kapt(androidCompiler)
+    }
+}
