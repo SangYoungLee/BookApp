882a2a7fc25282096893b02943bb9d006c5c428c
SangYeong Lee <lsy2014@gmail.com>
2023-11-27 23:19:26 +0900

    상세 화면 fetch 로직 추가

--- presentation/detail_book/src/main/java/com/syapp/bookapp/detail_book/contracts/DetailBookViewModel.kt
+++ presentation/detail_book/src/main/java/com/syapp/bookapp/detail_book/contracts/DetailBookViewModel.kt
@@ -3,7 +3,9 @@ package com.syapp.bookapp.detail_book.contracts
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import com.syapp.bookapp.core.base.BaseViewModel
-import com.syapp.bookapp.detail_book.contracts.DetailBookContract.*
+import com.syapp.bookapp.detail_book.contracts.DetailBookContract.DetailBookSideEffect
+import com.syapp.bookapp.detail_book.contracts.DetailBookContract.DetailBookViewEvent
+import com.syapp.bookapp.detail_book.contracts.DetailBookContract.DetailBookViewState
 import com.syapp.bookapp.domain.input.DetailBookInput
 import com.syapp.bookapp.domain.model.state.ActionState
 import com.syapp.bookapp.domain.usecase.GetDetailBookUseCase
@@ -22,12 +24,31 @@ class DetailBookViewModel @Inject constructor(
     private val isbn13: String = savedStateHandle[DetailBookContract.KEY_REQUEST_ID] ?: throw IllegalArgumentException()
 
     init {
-
+        fetchDetailBook()
     }
 
     private fun fetchDetailBook() {
         viewModelScope.launch {
-            getDetailBookUseCase.invoke(DetailBookInput(isbn13))
+            updateState {
+                copy(actionState = ActionState.contentLoading())
+            }
+
+            kotlin.runCatching {
+                val detailBook = getDetailBookUseCase.invoke(DetailBookInput(isbn13))
+
+                updateState {
+                    copy(
+                        actionState = ActionState.None,
+                        detailBook = detailBook
+                    )
+                }
+            }.onFailure {
+                updateState {
+                    copy(
+                        actionState = ActionState.Error(it)
+                    )
+                }
+            }
         }
     }
 }
