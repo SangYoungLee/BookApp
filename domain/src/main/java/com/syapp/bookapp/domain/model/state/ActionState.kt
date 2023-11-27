fa85ff8dfcb09e5041ad507eb6a4c8e8aa18ccc9
SangYeong Lee <lsy2014@gmail.com>
2023-11-27 21:23:47 +0900

    ActionState 필드 정의

--- domain/src/main/java/com/syapp/bookapp/domain/model/state/ActionState.kt
+++ domain/src/main/java/com/syapp/bookapp/domain/model/state/ActionState.kt
@@ -1,10 +1,19 @@
 package com.syapp.bookapp.domain.model.state
 
-sealed interface ActionState {
+sealed class ActionState {
 
-    data class Loading(val loadType: LoadType) : ActionState
+    data class Loading(val loadType: LoadType) : ActionState()
 
-    object None : ActionState
+    object None : ActionState()
 
-    data class Error(val cause: Throwable?) : ActionState
+    data class Error(val cause: Throwable?) : ActionState()
+
+    val isContentLoading: Boolean
+        get() = this is Loading && loadType == LoadType.CONTENT_LOADING
+
+    val isMoreLoading: Boolean
+        get() = this is Loading && loadType == LoadType.MORE_LOADING
+
+    val isError: Boolean
+        get() = this is Error
 }
