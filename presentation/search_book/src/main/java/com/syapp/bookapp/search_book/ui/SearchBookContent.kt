a3f4e0df3e785d00815fcbf883e31a2bda8384e7
SangYeong Lee <lsy2014@gmail.com>
2023-11-27 20:44:25 +0900

    Scaffold 적용 뎁스 수정

--- presentation/search_book/src/main/java/com/syapp/bookapp/search_book/ui/SearchBookContent.kt
+++ presentation/search_book/src/main/java/com/syapp/bookapp/search_book/ui/SearchBookContent.kt
@@ -1,32 +1,21 @@
 package com.syapp.bookapp.search_book.ui
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.syapp.bookapp.domain.model.Book
 
 @Composable
 fun SearchBookContent(
-    text: String,
-    onTextChanged: (String) -> Unit,
-    bookList: List<Book>
+    bookList: List<Book>,
+    modifier: Modifier,
 ) {
-    Scaffold(
-        topBar = {
-            SearchBookAppBar(text = text, onTextChanged = onTextChanged)
-        }
-    ) { paddingValues ->
-        LazyColumn(
-            modifier = Modifier.padding(
-                bottom = paddingValues.calculateBottomPadding()
-            )
-        ) {
-            items(bookList) { book ->
-                SearchBookItem(book = book)
-            }
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(bookList) { book ->
+            SearchBookItem(book = book)
         }
     }
 }
--- presentation/search_book/src/main/java/com/syapp/bookapp/search_book/ui/SearchBookScreen.kt
+++ presentation/search_book/src/main/java/com/syapp/bookapp/search_book/ui/SearchBookScreen.kt
@@ -1,8 +1,13 @@
 package com.syapp.bookapp.search_book.ui
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.syapp.bookapp.core.ui.ErrorScreen
 import com.syapp.bookapp.core.ui.LoadingScreen
@@ -16,17 +21,22 @@ fun SearchBookScreen(
 ) {
     val viewState by viewModel.viewState.collectAsState()
 
-    if (viewState.isLoading) {
-        LoadingScreen()
-    } else if (viewState.hasError) {
-        ErrorScreen(throwable = viewState.error)
-    } else {
-        SearchBookContent(
-            text = viewState.query,
-            onTextChanged = rememberLambda(key1 = viewModel) { text ->
+    Scaffold(
+        topBar = {
+            SearchBookAppBar(text = viewState.query, onTextChanged = rememberLambda(key1 = viewModel) { text ->
                 viewModel.setViewEvent(OnTextChanged(text))
-            },
-            bookList = viewState.bookList,
-        )
+            })
+        }
+    ) { paddingValues ->
+        if (viewState.isLoading) {
+            LoadingScreen()
+        } else if (viewState.hasError) {
+            ErrorScreen(throwable = viewState.error)
+        } else {
+            SearchBookContent(
+                bookList = viewState.bookList,
+                modifier = Modifier.padding(bottom = paddingValues.calculateBottomPadding())
+            )
+        }
     }
 }
